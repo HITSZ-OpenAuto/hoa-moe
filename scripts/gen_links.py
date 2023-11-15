7529315f12bb2f2e24199e5b5610a914b43338b7
xiln7 <xiln7@outlook.com>
2023-11-15 18:56:31 +0800

    styles: update links format and add token for req

--- scripts/gen_links.py
+++ scripts/gen_links.py
@@ -1,10 +1,11 @@
 import requests
 import argparse
 
-def list_files_in_repo(owner, repo, path=''):
+def list_files_in_repo(owner, repo, token, path=''):
     paths = []
     url = f'https://api.github.com/repos/{owner}/{repo}/contents/{path}'
-    response = requests.get(url)
+    headers = {'Authorization': f'token {token}'}
+    response = requests.get(url, headers=headers)
     if response.status_code == 200:
         contents = response.json()
         for content in contents:
@@ -20,22 +21,25 @@ def list_files_in_repo(owner, repo, path=''):
 
     return paths
 
-def save_files_list(owner, repo):
-     paths = list_files_in_repo(owner, repo)
-     prefix = f'https://github.com/HITSZ-OpenAuto/{repo}/raw/main/'
+def save_files_list(owner, repo, token):
+     paths = list_files_in_repo(owner, repo, token)
+     prefix = f'https://github.com/{owner}/{repo}/raw/main/'
      filtered_paths = [path for path in paths if path.endswith(('.pdf', '.zip'))]
      with open('result.txt', 'w') as file:
         for path in filtered_paths:
             full_path = prefix + path
-            file.write(full_path + '\n')
+            full_path_md = f'[{path}]({full_path})'
+            file.write(full_path_md + '<br>')
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Generate download links of files from a GitHub repository.")
     parser.add_argument("owner", help="GitHub repository owner")
     parser.add_argument("repo", help="GitHub repository name")
+    parser.add_argument("token", help="GitHub token")
 
     args = parser.parse_args()
     owner = args.owner
     repo = args.repo
+    token = args.token
     
-    save_files_list(owner, repo)
+    save_files_list(owner, repo, token)
