6824677fbd3713a12aae9f29c60af6bc22d14edd
oliver-wu <3531945226@qq.com>
2023-11-17 01:54:58 +0800

    a new way to display index and table of contents

--- themes/hextra/assets/js/back-to-top.js
+++ themes/hextra/assets/js/back-to-top.js
@@ -1,13 +1,27 @@
 // Back to top button
 
 document.addEventListener("DOMContentLoaded", function () {
-  const backToTop = document.querySelector("#backToTop");
-  if (backToTop) {
-    document.addEventListener("scroll", (e) => {
+  const backToTop = document.querySelectorAll("#backToTop");
+  backToTop.forEach(function (btt) {
+    document.addEventListener("scroll", function (e) {
       if (window.scrollY > 300) {
-        backToTop.classList.remove("opacity-0");
+        btt.classList.remove("opacity-0");
       } else {
-        backToTop.classList.add("opacity-0");
+        btt.classList.add("opacity-0");
+      }
+    });
+  });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  const switch_1 = document.getElementById("switchlabel");
+  if (switch_1) {
+    document.addEventListener("scroll", (e) => {
+      if (window.scrollY > document.scrollingElement.scrollHeight-document.scrollingElement.clientHeight-200) {
+        switch_1.classList.add("opacity-0");
+        console.log(switch_1.parentElement);
+      } else {
+        switch_1.classList.remove("opacity-0");
       }
     });
   }
