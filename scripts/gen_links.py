9f229cb1046141f0408dc956e79d26de3a5f7e82
xiln7 <xiln7@outlook.com>
2024-01-02 16:05:33 +0800

    Add support for .ipynb files in filtered_paths

--- scripts/gen_links.py
+++ scripts/gen_links.py
@@ -60,7 +60,7 @@ def list_files_in_repo(owner, repo, username, token, path=''):
 
 def save_files_list(owner, repo, username, token):
     paths = list_files_in_repo(owner, repo, username, token)
-    filtered_paths = [path for path in paths if path.endswith(('.pdf', '.zip', '.rar', '.7z', '.docx', '.doc'))]
+    filtered_paths = [path for path in paths if path.endswith(('.pdf', '.zip', '.rar', '.7z', '.docx', '.doc', '.ipynb'))]
     result_content = create_hugo_shortcode(filtered_paths, owner, repo)
     with open('result.txt', 'w') as file:
         file.write(result_content)     
