7c23817d2efe36f7fc7dedfc94c0f3ca0815f62d
longlin10086 <gnulonglin@gmail.com>
2024-04-24 02:55:58 +0800

    fix: remove empty boxes

--- scripts/gen_links.py
+++ scripts/gen_links.py
@@ -61,9 +61,8 @@ def list_files_in_repo(owner, repo, username, token, path=''):
 def save_files_list(owner, repo, username, token):
     paths = list_files_in_repo(owner, repo, username, token)
     filtered_paths = [path for path in paths if path.endswith(('.pdf', '.zip', '.rar', '.7z', '.docx', '.doc', '.ipynb', '.pptx', '.apkg', '.mp4', '.csv', 'xlsx'))]
-    if not filtered_paths:
-        result_content = ""
-    else:
+    result_content = ""
+    if filtered_paths:
         result_content = create_hugo_shortcode(filtered_paths, owner, repo)
     with open('result.txt', 'w') as file:
         file.write(result_content)     
