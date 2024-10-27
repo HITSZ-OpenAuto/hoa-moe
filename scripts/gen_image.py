fe2a1b050662a160f7c67f578518a40403208486
Kowyo <110339237+kowyo@users.noreply.github.com>
2024-10-27 15:58:56 +0800

    feat: refactor news module and ai image cover (#171)
    * feat: refactor news module and ai image cover
    
    * fix dependency error
    
    * add date info

---
+++ scripts/gen_image.py
@@ -0,0 +1,31 @@
+from openai import OpenAI
+import requests
+from PIL import Image
+
+def generate_image(api_key):
+    """Generate a random image of abstract art using DALL-E."""
+    print("Generating an ai-generated image...")
+    prompt = "a random image of abstract oil painting"
+    client = OpenAI(
+        # This is the default and can be omitted
+        api_key=api_key,
+        base_url="https://api.aihubmix.com/v1/"
+    )
+
+    response = client.images.generate(prompt=prompt)
+
+    # Extract the URL of the generated image
+    image_url = response.data[0].url
+    image_response = requests.get(image_url)
+
+    # Save the image to a file
+    if image_response.status_code == 200:
+        with open('generated_image.png', 'wb') as f:
+            f.write(image_response.content)
+        print("Image downloaded and saved as 'generated_image.png'")
+    else:
+        print("Failed to download the image")
+
+    img = Image.open("generated_image.png")
+    img = img.crop((0, 0, img.width, img.height // 2))
+    img.save("generated_image.png")
