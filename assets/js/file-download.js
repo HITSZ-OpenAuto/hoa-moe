b117ea1915679c253f4cd5df9f6f718dbe55c064
syhanjin <2819469337@qq.com>
2025-03-12 20:44:33 +0800

    fix(download): force download trigger and add progress bar

--- assets/js/file-download.js
+++ assets/js/file-download.js
@@ -1,35 +1,99 @@
 // download checked file
 
-function downloadFile(url, index) {
-    const iframe = document.createElement('iframe');
-    iframe.style.display = 'none';
-    iframe.src = url;
-    iframe.id = 'download_iframe_' + index;
-    // a.download = fileName;
-    document.body.appendChild(iframe);
-    // a.click();
-
+function triggerDownload(blob, fileName) {
+    const url = URL.createObjectURL(blob);
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = fileName || "download"; // 设置下载文件名
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+    setTimeout(() => URL.revokeObjectURL(url), 5000);
 }
 
-document.addEventListener("DOMContentLoaded", function() {
+async function downloadFile(url, setProgress) {
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error(`HTTP ERROR! CODE: ${response.status}`);
+        }
+        const contentLength = response.headers.get("Content-Length");
+        const total = parseInt(contentLength) || null;
+        let loaded = 0;
+
+        const reader = response.body.getReader();
+        const chunks = [];
+
+        while (true) {
+            const {done, value} = await reader.read();
+            if (done) break;
+            chunks.push(value);
+            loaded += value.length;
+
+            setProgress(total ? (loaded / total * 100) : 99);
+        }
+        setProgress(100);
+        const blob = new Blob(chunks);
+        triggerDownload(blob, decodeURIComponent(url.split('/').pop()));
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+document.addEventListener("DOMContentLoaded", function () {
     const downloadButton = document.querySelector('.hoa-filetree-download');
     downloadButton.addEventListener('click', () => {
         // get all checked file
-        let links = [];
         const files = document.querySelectorAll('.hoa-filetree-file');
         Array.from(files).forEach((file, index) => {
             const checkbox = file.querySelector('input');
-            if(checkbox.checked){
+            if (checkbox.checked) {
                 const link = file.querySelector('.hoa-filetree-download-link');
-                links.push(link);
-                downloadFile(link.href);
+                link.click();
             }
         });
-        setTimeout(() => {
-            links.forEach((url, index) => {
-                let iframe = document.getElementById('download_iframe_' + index);
-                document.body.removeChild(iframe);
-            });
-        }, 3000);
     });
+
+    const downloadFileButtons = document.querySelectorAll('.hoa-filetree-download-link');
+    downloadFileButtons.forEach(ele => ele.addEventListener('click', (e) => {
+        e.stopPropagation();
+        e.preventDefault();
+        // get link
+        const url = e.target.dataset.url;
+        // hide icon and show progress
+        e.target.style.display = 'none';
+        const progressWrapper = e.target.parentElement.querySelector(".hoa-filetree-download-progress-wrap");
+        progressWrapper.style.display = `block`;
+        const progressCircle = progressWrapper.querySelector(".hoa-filetree-download-progress");
+        const progressText = progressWrapper.querySelector(".hoa-filetree-download-progress-text");
+        const setProgress = (progress) => {
+            // console.log(progress)
+            const deg = progress * 3.6;
+            progressCircle.style.background = `conic-gradient(#1677ff 0, #1677ff ${deg}deg, transparent ${deg}deg, transparent 360deg)`;
+            progressText.innerText = progress.toFixed(0);
+        }
+        setProgress(0);
+        downloadFile(url, setProgress).then().catch();
+        console.log(url);
+    }));
+
+    /* preview
+    const previewFileButtons = document.querySelectorAll('.hoa-filetree-preview-link');
+    previewFileButtons.forEach(ele => ele.addEventListener('click', (e) => {
+        e.stopPropagation();
+        e.preventDefault();
+        // get link
+        const url = e.target.dataset.url;
+        const iframe = document.createElement('iframe');
+        iframe.style.position = "fixed";
+        iframe.style.height = "100vh";
+        iframe.style.width = "100vw";
+        iframe.style.left = "0";
+        iframe.style.top = "0";
+        iframe.style.zIndex = "9999";
+        iframe.src = url;
+        iframe.id = 'preview_iframe';
+        document.body.appendChild(iframe);
+    }));
+    */
 });
