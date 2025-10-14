369e8a5a42cdb411ac231238785432279e5cf292
Kowyo <kowyo@outlook.com>
2025-10-14 17:17:06 +0800

    make it a button

--- assets/js/core/hoa-width.js
+++ assets/js/core/hoa-width.js
@@ -5,22 +5,6 @@
     switcher.addEventListener('click', (e) => {
       e.preventDefault();
 
-      switcher.dataset.state = switcher.dataset.state === 'open' ? 'closed' : 'open';
-
-      toggleMenu(switcher);
     });
-  });
-
-  window.addEventListener("resize", () => widthSwitchers.forEach(resizeMenu))
-
-  // Dismiss language switcher when clicking outside
-  document.addEventListener('click', (e) => {
-    if (e.target.closest('.hoa-width-switcher') === null) {
-      widthSwitchers.forEach((switcher) => {
-        switcher.dataset.state = 'closed';
-        const optionsElement = switcher.nextElementSibling;
-        optionsElement.classList.add('hx:hidden');
-      });
-    }
   });
 })();
