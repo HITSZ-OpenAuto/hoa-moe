a6f4561e3c91fb0b5d97e03a829e369b196ecbc4
Kowyo <kowyo@outlook.com>
2025-11-20 16:46:36 +0800

    add eslint for markdown

---
+++ eslint.config.mjs
@@ -0,0 +1,18 @@
+// eslint.config.js
+import { defineConfig } from "eslint/config";
+import markdown from "@eslint/markdown";
+
+export default defineConfig([
+	{
+		ignores: ["themes/**", "content/docs/**"],
+	},
+	{
+		plugins: {
+			markdown,
+		},
+		extends: ["markdown/recommended"],
+		rules: {
+			"markdown/no-missing-label-refs": "off",
+		},
+	},
+]);
